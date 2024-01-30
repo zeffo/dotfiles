308a09c9a0b3064bba76b99dcc3459e48179296c
Aman Phatak <phatakaman@gmail.com>
2024-01-30 23:55:30 +0530

    feat: kitty rainbow tabs

---
+++ kitty/tab_bar.py
@@ -0,0 +1,117 @@
+# pyright: reportMissingImports=false
+# pyright: reportGeneralTypeIssues=false
+
+from kitty.fast_data_types import Color, Screen, get_options
+from catppuccin import Flavour
+from kitty.tab_bar import (
+    DrawData,
+    ExtraData,
+    TabBarData,
+    as_rgb,
+    draw_tab_with_powerline,
+    draw_title,
+)
+from kitty.utils import color_as_int
+import random
+
+opts = get_options()
+
+flavor = Flavour.mocha()
+fields = (
+    "pink",
+    "mauve",
+    "red",
+    "maroon",
+    "peach",
+    "yellow",
+    "green",
+    "teal",
+    "sky",
+    "sapphire",
+    "blue",
+    "lavender",
+)
+
+
+def cai(color):
+    return int(
+        "".join(["{:02x}".format(x) for x in (color.r, color.g, color.b, color.a)]), 16
+    )
+
+
+colors = [Color(*getattr(flavor, field).rgb, alpha=2) for field in fields]
+
+
+powerline_symbols = {
+    "slanted": ("", "╱"),
+    "round": ("", ""),
+}
+
+salt = random.randint(0, len(colors) - 1)
+
+
+def draw_tab(
+    draw_data: DrawData,
+    screen: Screen,
+    tab: TabBarData,
+    before: int,
+    max_tab_length: int,
+    index: int,
+    is_last: bool,
+    extra_data: ExtraData,
+) -> int:
+    index = index - 1 + salt
+    cur_color = colors[index % len(colors)]
+    tab_bg = cai(cur_color)
+    tab_fg = screen.cursor.fg
+    default_bg = as_rgb(int(draw_data.default_bg))
+    if extra_data.next_tab:
+        # next_tab_bg = as_rgb(draw_data.tab_bg(extra_data.next_tab))
+        next_tab_bg = cai(colors[(index + 1) % len(colors)])
+        needs_soft_separator = next_tab_bg == tab_bg
+    else:
+        next_tab_bg = default_bg
+        needs_soft_separator = False
+
+    separator_symbol, soft_separator_symbol = powerline_symbols.get(
+        draw_data.powerline_style, ("", "")
+    )
+    min_title_length = 1 + 2
+    start_draw = 2
+
+    if screen.cursor.x == 0:
+        screen.cursor.bg = tab_bg
+        screen.draw(" ")
+        start_draw = 1
+
+    screen.cursor.bg = tab_bg
+    if min_title_length >= max_tab_length:
+        screen.draw("…")
+    else:
+        draw_title(draw_data, screen, tab, index, max_tab_length)
+        extra = screen.cursor.x + start_draw - before - max_tab_length
+        if extra > 0 and extra + 1 < screen.cursor.x:
+            screen.cursor.x -= extra + 1
+            screen.draw("…")
+
+    if not needs_soft_separator:
+        screen.draw(" ")
+        screen.cursor.fg = tab_bg
+        screen.cursor.bg = next_tab_bg
+        screen.draw(separator_symbol)
+    else:
+        prev_fg = screen.cursor.fg
+        if tab_bg == tab_fg:
+            screen.cursor.fg = default_bg
+        elif tab_bg != default_bg:
+            c1 = draw_data.inactive_bg.contrast(draw_data.default_bg)
+            c2 = draw_data.inactive_bg.contrast(draw_data.inactive_fg)
+            if c1 < c2:
+                screen.cursor.fg = default_bg
+        screen.draw(f" {soft_separator_symbol}")
+        screen.cursor.fg = prev_fg
+
+    end = screen.cursor.x
+    if end < screen.columns:
+        screen.draw(" ")
+    return end

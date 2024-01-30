c4858042a017dda158968272c973ae49c2a4fe0f
Aman Phatak <phatakaman@gmail.com>
2024-01-30 17:56:32 +0530

    feat: kitty tab bars and stuff

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash 
 
 /usr/bin/prismatik &    # Prismatik (For Ambient Lighting)
-
-
-alac="alacritty --hold -e"
-
-alacritty="alacritty"
-sxhkd="sxhkd"
-bottom="$alac btm"
-neovim="$alac nvim"
-
-$neovim &
 picom -b
-$sxhkd &
-$bottom &
-$alacritty &
--- qtile/core/settings.py
+++ qtile/core/settings.py
@@ -1,8 +1,8 @@
 from catppuccin import Flavour
 
 palette = Flavour.mocha()
-terminal: str = "alacritty"  # Default terminal
+terminal: str = "kitty"  # Default terminal
 mod: str = "mod4"  # Mod (super) key
-wmname: str = "aman's super hot WM"  # WM Name
+wmname: str = ":3 WM"  # WM Name
 
 __all__ = ("terminal", "mod", "wmname", "palette")

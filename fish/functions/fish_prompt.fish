0233a1f0f7aae4c859c871c25b34262c06f2bafd
Aman Phatak <phatakaman@gmail.com>
2024-02-03 20:38:35 +0530

    feat: update fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,19 +1,45 @@
 function fish_prompt
-    set_color $fish_color_lavender
-    echo -n "┌── "
-    set_color -o $fish_color_pink # f5c2e7
-    if set -q SSH_TTY
-        set_color -o $fish_color_sky # 89dceb
-    end
-    echo -n $USER 
+    # set_color $fish_color_lavender
+    # echo -n "┌──"
+    # # set_color $fish_color_lavender
+    # echo -n ""
+    # set_color -o --background $fish_color_lavender # f5c2e7
+    # set_color $fish_color_crust
+    # if set -q SSH_TTY
+    #     set_color -o $fish_color_sky # 89dceb
+    # end
+    # echo -n  $USER 
+    # set_color normal
+    # set_color $fish_color_lavender
+    # echo -n ""
+    # set_color $fish_color_mauve 
+    # echo -n "─ ─" #      
+    # set_color $fish_color_teal 
+    # echo (prompt_pwd)
+    # set_color $fish_color_lavender
+    # # echo "│"
+    # echo -n '╰─ '
+    # # set_color f5c2e7
+    set_color $fish_color_pink
+    echo -n ""
+    set_color -o --background $fish_color_pink
+    set_color $fish_color_crust
+    echo -n " "
+    set_color --background $fish_color_mauve
+    set_color $fish_color_pink
+    echo -n " " 
+    set_color $fish_color_base
+    echo -n $USER  
     set_color normal
-    set_color $fish_color_mauve 
-    echo -n "   " #      
-    set_color $fish_color_teal 
-    echo (prompt_pwd)
-    set_color $fish_color_lavender
-    echo -n '╰─ '
-    # set_color f5c2e7
+    set_color $fish_color_mauve
+    set_color --background $fish_color_teal
+    echo -n " " 
+    set_color $fish_color_surface0
+    echo -n (prompt_pwd)
+    set_color normal 
+    set_color $fish_color_teal
+    echo -n " " 
+
 end
 
 

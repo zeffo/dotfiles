6386d41ba961ecbe0fb2c1e18b12c98c2dc0a227
Aman Phatak <aman@zeffo.me>
2024-02-18 22:20:23 +0530

    dump

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,22 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     atuin init fish | source
+    zoxide init fish | source
+    set -x EDITOR nvim
+    set -x BAT_THEME Catppuccin-mocha
+
+    alias cd="z" 
+    alias fox='firefox-developer-edition'
+    alias ls='eza --icons=always --no-quotes --group-directories-first --hyperlink'
+    alias grep='ag'
+    alias pdf="termpdf.py"
+    alias cat="bat"
+    alias spire="spotify_player"
+
+    pokeget random --hide-name; echo;
 end
 
-set -x EDITOR nvim
-set -x BAT_THEME Catppuccin-mocha
 
-alias hx="helix"
-alias fox='firefox-developer-edition'
-alias ls='eza --icons=always --no-quotes --group-directories-first --hyperlink'
-alias grep='ag'
-alias pdf="termpdf.py"
-alias cat="bat"
-alias spire="spotify_player"
-
-pokeget random --hide-name; echo;
 
 
 

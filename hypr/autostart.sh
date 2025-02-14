d86c04c5334b6fb4d9f41c6c18bdff330d3798d4
Aman Phatak <aman@zeffo.me>
2025-02-14 14:21:50 +0530

    feat: switch to fuzzel from wofi

--- hypr/autostart.sh
+++ hypr/autostart.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-
-
 waybar &
 hyprctl setcursor catppuccin-mocha-pink-cursors 32 # for some reason, hyprcursor 32 == gtk 24
 blueman-applet &
@@ -15,9 +13,9 @@ swaync &
 
 # swww
 swww kill
-swww init
+# swww init
 swww img ~/.config/hypr/wallpapers/blahaj.png -o DP-2 --transition-fps 255 
-wallpapers=("pixelmountain.gif" "pixeltrain.gif") 
+wallpapers=("pixelmountain.gif" "pixeltrain.gif" "sakura.gif") 
 size=${#wallpapers[@]}
 index=$(($RANDOM % $size))
 swww img ~/.config/hypr/wallpapers/${wallpapers[$index]} -o HDMI-A-1 --transition-fps 255

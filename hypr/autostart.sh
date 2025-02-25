82aa93b0b177ac6f2867bc5a78e882f3cde2f849
Aman Phatak <aman@zeffo.me>
2025-02-26 00:24:53 +0530

    feat: hyprcursor sizes fixed

--- hypr/autostart.sh
+++ hypr/autostart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 waybar &
-hyprctl setcursor catppuccin-mocha-pink-cursors 32 # for some reason, hyprcursor 32 == gtk 24
+hyprctl setcursor catppuccin-mocha-pink-cursors 24
 blueman-applet &
 
 wl-paste --watch cliphist store &
@@ -11,9 +11,8 @@ killall swaync
 pkill swaync
 swaync &
 
-# swww
 swww kill
-# swww init
+swww-daemon &
 swww img ~/.config/hypr/wallpapers/blahaj.png -o DP-2 --transition-fps 255 
 wallpapers=("pixelmountain.gif" "pixeltrain.gif" "sakura.gif") 
 size=${#wallpapers[@]}
